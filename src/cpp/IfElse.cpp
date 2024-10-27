d95bb9db56df5b232d6848df7085c63a606952fd
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-28 00:08:12 +0530

    yaya

--- src/cpp/IfElse.cpp
+++ src/cpp/IfElse.cpp
@@ -2,20 +2,25 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     // int package;
     // cin >> package;
     // if(package > 10) {
-    //         cout << "Accepted";    
+    //         cout << "Accepted";
     // } else {
     //     cout << "Rejected";
     // }
 
     int marks;
+    cout << "Enter your marks: ";
     cin >> marks;
-    if(marks > 33) {
+    if (marks > 33)
+    {
         cout << "Pass";
-    } else {
+    }
+    else
+    {
         cout << "Fail";
     }
     return 0;
