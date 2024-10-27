5ee7bdb38b6611a74aa14abfbc94eb453a54b0df
Adarsh Jha <adarsh2005j@gmail.com>
2024-10-27 17:36:08 +0530

    data type cpp

---
+++ src/cpp/DataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    a = 10;
+    cout << "Value of a is: " << a << endl;
+
+    char ch = 'a';
+    cout << "Value of ch is: " << ch << endl;
+
+    double d = 69.6969;
+    cout << "Value of d is: " << d << endl;
+
+    bool b = false;
+    cout << "Value of b is: " << b << endl;
+    return 0;
+}
