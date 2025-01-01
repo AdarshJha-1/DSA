f35e0c336c8bd3f9f2a0b9f2b52d25dc0b9d207d
Adarsh Jha <adarsh2005j@gmail.com>
2025-01-01 18:04:04 +0530

    sqrt binary search

--- src/searching/binarySearchQuestions/Sqrt.cpp
+++ src/searching/binarySearchQuestions/Sqrt.cpp
@@ -4,22 +4,24 @@ using namespace std;
 
 
 int findSquareRoot(int x) {
-    int s = 0;
-    int e = x;
-    int ans = -1;
-    while (s <= e)
-    {
-        int mid = s + (e - s) / 2;
-        if (mid * mid == x) {
-            return mid;
-        } else if (mid * mid > x) {
-            e = mid - 1;
-        } else {
-            s = mid + 1;
-            ans = mid;
+    if (x < 2) {
+            return x;
         }
-    }
-    return ans;   
+        int s = 0;
+        int e = x;
+        int ans = -1;
+        while(s <= e) {
+            int mid = s + (e - s) / 2;
+            if (mid == x/mid) {
+                return mid;
+            } else if (mid > x/mid) {
+                e = mid - 1;
+            } else {
+                ans = mid;
+                s = mid + 1;
+            }
+        }
+        return ans; 
 }
 
 
