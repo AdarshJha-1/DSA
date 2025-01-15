6a8fbda5044ec15c43fba80c2673e642ecda6efc
Adarsh Jha <adarsh2005j@gmail.com>
2025-01-15 20:55:41 +0530

    more more more

---
+++ src/searching/binarySearchQuestions/MaxPageMin.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int student = 1;
+    int allocatedPage = 0;
+    for(int i = 0; i < n; i++) {
+        if(allocatedPage + arr[i] <= mid) {
+            allocatedPage += arr[i];
+        }else {
+            student++;
+            if(student > m) {
+                return false;
+            }
+            allocatedPage = arr[i];
+        }
+    }
+    return true;
+}
+
+int maxPageMin(vector<int> arr, int n, int m) {
+    int start = 0;
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    int end = sum;
+    int ans = -1;
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+        if(isPossible(arr, n, m, mid)) {
+            ans = mid;
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40};
+    int n = arr.size();
+    int m = 2;
+
+    int ans = maxPageMin(arr, n, m);
+    cout << ans << endl;
+
+}
