c9f34f370d1c129915ad2dab6340da6efe6e3255
Adarsh Jha <adarsh2005j@gmail.com>
2025-01-18 18:44:13 +0530

    reimplement

--- src/array/KeyboardRow.cpp
+++ src/array/KeyboardRow.cpp
@@ -4,17 +4,33 @@
 
 using namespace std;
 
-vector<string> findWords(vector<string> &words)
-{
-    string fRow = "qwertyuiop";
-    string sRow = "asdfghjkl";
-    string tRow = "zxcvbnm";
+// bool inFirstRow(string word, string fRow) {
+//     for(int i = 0; i < fRow.length(); i++ ) {
+//         // word.find
+//     }
+//     return false;
+// }
 
-    
-}
+// vector<string> findWords(vector<string> &words)
+// {
+//     string fRow = "qwertyuiop";
+//     string sRow = "asdfghjkl";
+//     string tRow = "zxcvbnm";
+
+//     vector<string> ans;
+//     string currentWord = "";
+//     for(int i = 0; i <words.size(); i++) {
+//         currentWord = words[i];
+        
+//     }
+
+//  return ans;   
+// }
 
 int main()
 {
-
+    string wo = "Hello";
+    int i = wo.find();
+    cout << i;
     return 0;
 }
--- src/searching/binarySearchQuestions/MaxPageMin.cpp
+++ src/searching/binarySearchQuestions/MaxPageMin.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
+int maxInArray(vector<int> arr) {
+    int maxEle = INT_MIN;
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] > maxEle); {
+            maxEle = arr[i];
+        }
+    }
+    return maxEle;
+}
+
+int sumOfAllEleInArray(vector<int> arr) {
+    int sum = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
 bool isPossible(vector<int> arr, int n, int m, int mid) {
-    int student = 1;
-    int allocatedPage = 0;
+    int pageAssign = 0;
+    int students = 1;
     for(int i = 0; i < n; i++) {
-        if(allocatedPage + arr[i] <= mid) {
-            allocatedPage += arr[i];
+        if(pageAssign + arr[i] <= mid) {
+            pageAssign += arr[i];
         }else {
-            student++;
-            if(student > m) {
+                students++;
+            if(students > m) {
                 return false;
             }
-            allocatedPage = arr[i];
+                pageAssign = arr[i];
         }
     }
     return true;
 }
 
-int maxPageMin(vector<int> arr, int n, int m) {
-    int start = 0;
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        sum += arr[i];
-    }
-    int end = sum;
-    int ans = -1;
+int maxMinPag(vector<int> arr, int n, int m) {
+    int start = maxInArray(arr);
+    int end =  sumOfAllEleInArray(arr);
+    int mid, ans;
     while(start <= end) {
-        int mid = start + (end - start) / 2;
+        mid = start + (end - start) / 2;
         if(isPossible(arr, n, m, mid)) {
             ans = mid;
             end = mid - 1;
@@ -41,11 +56,10 @@ int maxPageMin(vector<int> arr, int n, int m) {
 }
 
 int main() {
-    vector<int> arr = {10, 20, 30, 40};
-    int n = arr.size();
+    vector<int> arr = {12, 34, 67, 90};
+    int n = 4;
     int m = 2;
-
-    int ans = maxPageMin(arr, n, m);
+    int ans = maxMinPag(arr, n, m);
     cout << ans << endl;
-
+    return 0;
 }
