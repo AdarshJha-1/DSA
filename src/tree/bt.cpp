d953fef14198e917a8aa2d249c5a989645c014de
Adarsh Jha <adarsh2005j@gmail.com>
2025-08-23 19:43:01 +0530

    binary tree

--- src/tree/bt.cpp
+++ src/tree/bt.cpp
@@ -17,7 +17,7 @@ public:
 class BT {
 
 public:
-Node* root;
+    Node* root;
     void createBT() {
         std::queue<Node*> q;
         int d;
@@ -47,12 +47,48 @@ Node* root;
         }
         std::cout << "Binary Tree Created Successfully." << std::endl;
     }
+
+
+    Node* createBt() {
+        int data;
+        std::cout << "Enter the element: ";
+        std::cin >> data;
+        if(data == -1) return nullptr;
+        Node* node = new Node(data);
+        node->left = createBt();
+        node->right = createBt();
+        return node;
+    }
+
+    void preorder(Node* root) {
+        if(root == nullptr) return;
+        std::cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    void postorder(Node* root) {
+        if(root == nullptr) return;
+        postorder(root->left);
+        postorder(root->right);
+        std::cout << root->data << " ";
+    }
+    void inorder(Node* root) {
+        if(root == nullptr) return;
+        inorder(root->left);
+        std::cout << root->data << " ";
+        inorder(root->right);
+    }
 };
 
 int main() {
 
     BT b;
-    b.createBT();
-
+    b.root = b.createBt();
+    b.preorder(b.root);
+    std::cout << std::endl;
+    b.postorder(b.root);
+    std::cout << std::endl;
+    b.inorder(b.root);
+    std::cout << std::endl;
     return 0;
 }
