8806a70100382254f74a49bc1a47bec78c9737d5
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-10 17:22:14 +0530

    geek tree

--- src/tree/BottomViewBT.cpp
+++ src/tree/BottomViewBT.cpp
@@ -9,30 +9,47 @@ struct Node
 
 class Solution {
   public:
-    
+  
     void lr(Node* root, int curr, int &l, int &r) {
         if(!root) return;
-        l = min(curr, l);
-        r = max(curr, r);
+        l = min(l, curr);
+        r = max(r, curr);
+        
         lr(root->left, curr - 1, l, r);
         lr(root->right, curr + 1, l, r);
-    } 
-  
-    void bV(Node* root, vector<int>&ans, vector<int>&lvl, int pos, int clvl) {
-        if(!root) return;
-        if(clvl > lvl[pos]) {
-            ans[pos] = root->data;
-            lvl[pos] = clvl;
-        }
-        bV(root->left, ans, lvl, pos - 1, clvl + 1);
-        bV(root->right, ans, lvl, pos + 1, clvl + 1);
     }
+    
     vector<int> bottomView(Node *root) {
         int l = 0, r = 0;
         lr(root, 0, l, r);
         vector<int> ans(r - l + 1);
-        vector<int> lvl(r - l + 1, INT_MIN);
-        bV(root, ans, lvl, (-1 * l), 0);
+        vector<int> lvls(r- l + 1, -1);
+        queue<Node*> q;
+        q.push(root);
+        queue<int> idx;
+        idx.push(-1 * l);
+
+        int currLvl = 0;
+        while(!q.empty()) {
+            Node* temp = q.front();
+            q.pop();
+            int i = idx.front();
+            idx.pop();
+            if(currLvl > lvls[i]) {
+                ans[i] = temp->data;
+            }
+            
+            if(temp->left) {
+                q.push(temp->left);
+                idx.push(i - 1);
+            }
+             if(temp->right) {
+                q.push(temp->right);
+                idx.push(i + 1);
+            }A
+            currLvl++;
+        }
+        
         return ans;
     }
 };
