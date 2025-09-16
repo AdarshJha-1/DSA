d41f2052a38d0495f01b2d4a9e1d8198ccf6d597
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-16 12:11:30 +0530

    leetcode array tle

--- src/array/3Sum.cpp
+++ src/array/3Sum.cpp
@@ -1,25 +1,30 @@
-#include <iostream>
-#include <vector>
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        vector<vector<int>> ans;
+        set<vector<int>> s;
 
-using namespace std;
+        for (int i = 0; i < arr.size() - 2; i++) {
+            for (int j = i + 1; j < arr.size() - 1; j++) {
+                vector<int> temp;
+                for (int k = j + 1; k < arr.size(); k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        temp.push_back(arr[i]);
+                        temp.push_back(arr[j]);
+                        temp.push_back(arr[k]);
+                        sort(temp.begin(), temp.end());
+                        s.insert(temp);
+                    }
+                    temp = {};
+                }
+            }
+        }
 
-int sum3v1(vector<int> arr, int x) {
-  for(int i = 0; i < arr.size() - 2; i++) {
-    for(int j  = i + 1; j < arr.size() - 1; j++ )  {
-      for(int k = j + 1; k < arr.size(); k++) {
-	if(arr[i] + arr[j] + arr[k] == x) {
-	  return 1;
-	}
-      }
+        for (const auto& vec : s) {
+            if(vec.size() != 0) {
+                ans.push_back(vec);
+            }
+        }
+        return ans;
     }
-  }
-  return 0;
-}
-
-int main() {
-  vector<int> arr = {1,4,45,6,10,8};
-  int x = 13;
-  int ans = sum3v1(arr, x); 
-  cout << ans << endl;
-  return 0;
-}
+};
