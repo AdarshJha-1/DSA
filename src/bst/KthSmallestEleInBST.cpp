d57dc342ed9b63f6ec93da2696e01e103f88dcf5
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-17 18:48:09 +0530

    leetcode bst

--- src/bst/KthSmallestEleInBST.cpp
+++ src/bst/KthSmallestEleInBST.cpp
@@ -24,3 +24,37 @@ public:
         return temp[k - 1];
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, int k, int& ans, int &i) {
+        if (!root) return;
+        inorder(root->left, k, ans, i);
+        i++;
+        if (k == i) {
+            ans = root->val;
+            return;
+        }
+        inorder(root->right, k, ans, i);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int ans = -1;
+        int i = 0;
+        inorder(root, k, ans, i);
+        return ans;
+    }
+};
