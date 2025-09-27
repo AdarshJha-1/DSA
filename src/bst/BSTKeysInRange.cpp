9a2043c12433fd7dd003dfcd9fd45ad0edf77aca
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-27 17:42:43 +0530

    geek bst

--- src/bst/BSTKeysInRange.cpp
+++ src/bst/BSTKeysInRange.cpp
@@ -1,4 +1,4 @@
-lass Solution {
+class Solution {
   public:
  
     void sol(Node* root, int l, int h, vector<int> &ans) {
@@ -8,6 +8,32 @@ lass Solution {
             ans.push_back(root->data);
         }
         sol(root->right, l, h, ans);
+    }
+
+    vector<int> printNearNodes(Node *root, int low, int high) {
+        vector<int> ans;
+        sol(root, low, high, ans);
+        return ans;
+    }
+};
+
+class Solution {
+  public:
+ 
+    void sol(Node* root, int l, int h, vector<int> &ans) {
+        if(!root) return;
+        
+        if(root->data > l && root->data > h) {
+            sol(root->left, l, h, ans);
+        } else if(root->data < l && root->data < h) {
+            sol(root->right, l, h, ans);
+        } else {
+            sol(root->left, l, h, ans);
+            if(root->data >= l && root->data <= h) {
+                ans.push_back(root->data);
+            }
+            sol(root->right, l, h, ans);
+        }
     }
 
     vector<int> printNearNodes(Node *root, int low, int high) {
