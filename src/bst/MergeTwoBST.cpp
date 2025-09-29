d7844d68a53baebd9b2dc7f68782b8cd23678121
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-29 17:06:08 +0530

    geek BST

--- src/bst/MergeTwoBST.cpp
+++ src/bst/MergeTwoBST.cpp
@@ -80,3 +80,82 @@ class Solution {
         return ans;
     }
 };
+
+
+
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    vector<int> merge(Node *root1, Node *root2) {
+        vector<int> ans;
+        stack<Node*> s1, s2;
+        while(root1) {
+            s1.push(root1);
+            root1 = root1->left;
+        }
+        
+        while(root2) {
+            s2.push(root2);
+            root2 = root2->left;
+        }
+        
+        
+        while(!s1.empty() && !s2.empty()) {
+            if(s1.top()->data < s2.top()->data) {
+                ans.push_back(s1.top()->data);
+                Node* temp = s1.top()->right;
+                s1.pop();
+                while(temp){
+                    s1.push(temp);
+                    temp = temp->left;
+                }
+            } else {
+                ans.push_back(s2.top()->data);
+                Node* temp = s2.top()->right;
+                s2.pop();
+                while(temp){
+                    s2.push(temp);
+                    temp = temp->left;
+                }
+            }
+        }
+        
+        while(!s1.empty()) {
+            Node* temp = s1.top();
+            s1.pop();
+            ans.push_back(temp->data);
+            temp = temp->right;
+            while(temp) {
+                s1.push(temp);
+                temp = temp->left;
+            }
+        }
+        
+        while(!s2.empty()) {
+            Node* temp = s2.top();
+            s2.pop();
+            ans.push_back(temp->data);
+            temp = temp->right;
+            while(temp) {
+                s2.push(temp);
+                temp = temp->left;
+            }
+        }
+        
+        return ans;
+    }
+};
