40f8bc2c6fa1aba3c568951388aa5416a313554c
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-29 16:24:40 +0530

    geek BST

--- src/bst/MergeTwoBST.cpp
+++ src/bst/MergeTwoBST.cpp
@@ -46,3 +46,37 @@ class Solution {
         return ans;
     }
 };
+
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    
+    void ino(Node* root, vector<int> &arr) {
+        if(!root) return;
+        ino(root->left, arr);
+        arr.push_back(root->data);
+        ino(root->right, arr);
+    }
+    vector<int> merge(Node *root1, Node *root2) {
+        vector<int> ans;
+        ino(root1, ans);
+        ino(root2, ans);
+        
+       sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
