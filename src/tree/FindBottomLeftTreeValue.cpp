1dfeab6ed8f2b35c5a2e88755ae149be34829f20
Adarsh Jha <adarsh2005j@gmail.com>
2025-09-30 22:38:53 +0530

    leetcode tree

--- src/tree/FindBottomLeftTreeValue.cpp
+++ src/tree/FindBottomLeftTreeValue.cpp
@@ -33,3 +33,35 @@ public:
         return ans;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void sol(TreeNode* root, int cl, int &ml, TreeNode* &ans) {
+        if(!root) return;
+        sol(root->left, cl + 1, ml, ans);
+        if(cl > ml) {
+            ans = root;
+            ml = cl;
+        }
+        sol(root->right, cl + 1, ml, ans);
+    } 
+    int findBottomLeftValue(TreeNode* root) {
+        TreeNode* ans = root;
+        int ml = 0;
+        sol(root, 0, ml, ans);
+        return ans->val;
+    }
+};
