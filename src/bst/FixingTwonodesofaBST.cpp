fecd86a1407daac0100981f8ec4945eb20923e2f
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-02 12:33:21 +0530

    geek BST mind f* :)

---
+++ src/bst/FixingTwonodesofaBST.cpp
@@ -0,0 +1,63 @@
+/*
+class Node
+{
+    public:
+    int data;
+    Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void correctBST(Node* root) {
+        Node *curr = root, *prev = nullptr, *f = nullptr, *s = nullptr;
+        while(curr) {
+            if(!curr->left) {
+                if(prev) {
+                    if(prev->data > curr->data) {
+                        if(!f) {
+                            f = prev;
+                            s = curr;
+                        } else {
+                            s = curr;
+                        }
+                    }
+                }
+                prev = curr;
+                curr = curr->right;
+            } else {
+                Node *temp = curr->left;
+                while(temp->right && temp->right != curr) {
+                    temp = temp->right;
+                }
+                
+                if(!temp->right) {
+                    temp->right = curr;
+                    curr = curr->left;
+                } else {
+                    temp->right = nullptr;
+                    if(prev) {
+                        if(prev->data > curr->data) {
+                        if(!f) {
+                            f = prev;
+                            s = curr;
+                        } else {
+                            s = curr;
+                        }
+                    }
+                    }
+                        prev = curr;
+                        curr = curr->right;
+                }
+            }
+        }
+        if(f && s) {
+            swap(f->data, s->data);
+        }
+    }
+};
