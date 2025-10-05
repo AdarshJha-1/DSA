78f63f997103382d005a8089ff751c770bc8820b
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-05 22:21:36 +0530

    codeforce c1

---
+++ codeforces/Elfucho.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int ans = 2 * (n - 1);
+        cout << ans << endl;
+    }
+    return 0;
+}
