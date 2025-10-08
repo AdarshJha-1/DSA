105b0a7e207fae5dd7a6925a9d13b8bd6b657c9d
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-08 15:54:15 +0530

    leetcode heap

---
+++ src/heap/TakeGiftsFromTheRichestPile.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq;
+        for(int &g : gifts) pq.push(g);
+        while(k-- && !pq.empty()) {
+            int t = pq.top();
+            pq.pop();
+            pq.push(floor(sqrt(t)));
+        }
+        long long sum = 0;
+        while(!pq.empty()) {
+            sum += pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
+
