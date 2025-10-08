23176cc35607acbc61ab37f9b91f11016f80d901
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-08 20:07:17 +0530

    geek heap

--- src/heap/MergeTwoBinaryMaxHeaps.cpp
+++ src/heap/MergeTwoBinaryMaxHeaps.cpp
@@ -19,3 +19,34 @@
             return ans;
         }
     };
+
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            ans.push_back(a[i]);
+        }
+        
+        for(int i = 0; i < m; i++) {
+            ans.push_back(b[i]);
+        }
+        
+        int s = ans.size();
+        for(int i = (s / 2) - 1; i >=0; i--) {
+            int t = i;
+            while(t * 2 + 1 < s) {
+                int lg = t, l = t * 2 + 1, r = t * 2 + 2;
+                if(l < s && ans[l] > ans[lg]) lg = l;
+                if(r < s && ans[r] > ans[lg]) lg = r;
+                if(lg == t) break;
+                swap(ans[lg], ans[t]);
+                t = lg;
+            }
+        }
+        
+        return ans;
+    }
+};
