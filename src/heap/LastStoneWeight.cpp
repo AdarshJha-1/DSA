b3de4baa63b480b198c1af6e6daa647168d2b764
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-08 15:25:15 +0530

    leetcode array heap mix o(nlong) sol

--- src/heap/LastStoneWeight.cpp
+++ src/heap/LastStoneWeight.cpp
@@ -12,3 +12,20 @@ public:
         return stones[n - 2];
     }
 };
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto &s : stones) pq.push(s);
+        while(pq.size() != 1) {
+            int f = pq.top();
+            pq.pop();
+            int s = pq.top();
+            pq.pop();
+            pq.push(abs(f - s));
+        }
+
+        return pq.top();
+    }
+};
