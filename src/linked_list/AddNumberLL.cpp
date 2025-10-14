661392ee3bb16bef37a9c7aa2797d84323e86888
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-14 14:13:31 +0530

    geek LL optimal

--- src/linked_list/AddNumberLL.cpp
+++ src/linked_list/AddNumberLL.cpp
@@ -38,3 +38,82 @@ class Solution {
         return h;
     }
 };
+
+
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    Node* reverseLL(Node* h) {
+        Node* c = h, *p = nullptr, *n = nullptr;
+        while(c) {
+            n = c->next;
+            c->next = p;
+            p = c;
+            c = n;
+        }
+        
+        return p;
+    }
+    
+    Node* addTwoLists(Node* head1, Node* head2) {
+        head1 = reverseLL(head1);
+        head2 = reverseLL(head2);
+        
+        int c = 0;
+        Node* ans = nullptr, *temp = nullptr;
+        
+        while(head1 && head2) {
+            int sum = head1->data + head2->data + c;
+            if(!ans) {
+                ans = new Node(sum%10);
+                temp = ans;
+                c = sum / 10;
+            } else {
+                temp->next = new Node(sum%10);
+                c = sum / 10;
+                temp = temp->next;
+            }
+            
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        
+        while(head1)  {
+            int sum = head1->data + c;
+            temp->next = new Node(sum%10);
+            c = sum / 10;
+            temp = temp->next;
+            head1 = head1->next;
+        }
+        
+         while(head2)  {
+            int sum = head2->data + c;
+            temp->next = new Node(sum%10);
+            c = sum / 10;
+            temp = temp->next;
+            head2 = head2->next;
+        }
+        
+        while(c) {
+            temp->next = new Node(c%10);
+            c /= 10;
+        }
+        ans = reverseLL(ans);
+        while(ans->data == 0) {
+            ans = ans->next;
+        }
+        return ans;
+    }
+};
