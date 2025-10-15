fe0bf12c8a19152e2f0b1231dd64a5a43b10e8f2
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-15 13:55:51 +0530

    geek ll TLE

---
+++ src/linked_list/MergeKSortedLL.cpp
@@ -0,0 +1,49 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+    Node *merge(Node *h1, Node *h2) {
+        Node *head = new Node(0);
+        Node *tail = head;
+        while(h1 && h2) {
+            if(h1->data <= h2->data) {
+                tail->next = h1;
+                h1 = h1->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            } else {
+                tail->next = h2;
+                h2 = h2->next;
+                tail = tail->next;
+                tail->next = nullptr;
+            }
+        }
+        if(h1) {
+            tail->next = h1;
+        }
+        if(h2) {
+            tail->next = h2;
+        }
+        return head->next;
+    }
+  
+    Node* mergeKLists(vector<Node*>& arr) {
+        Node *root = arr[0];
+        for(int i = 1; i < arr.size(); i++) {
+            root = merge(root, arr[i]);
+        }
+        return root;
+    }
+};
