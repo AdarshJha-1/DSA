ab23f18bb47ae07d2c9be67468f6df5ce10ef029
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-15 11:54:19 +0530

    leetcode array, overflow

--- src/array/ReversePairs.cpp
+++ src/array/ReversePairs.cpp
@@ -10,3 +10,15 @@ public:
         return count;
     }
 };
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] > nums[j] * 2) count++;
+            }
+        }
+        return count;
+    }
+};
