11aad545106f062809f07167b2fb0952ae49ce5e
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-15 17:45:54 +0530

    geek ll hard :))

--- src/linked_list/CloneListWithNextAndRandom.cpp
+++ src/linked_list/CloneListWithNextAndRandom.cpp
@@ -58,3 +58,68 @@ class Solution {
         return root->next;
     }
 };
+
+
+
+
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* random;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* cloneLinkedList(Node* head) {
+        // code here
+        Node *c1 = head, *chead = new Node(0), *c2 = chead;
+        while(c1) {
+            c2->next = new Node(c1->data);
+            c2 = c2->next;
+            c1 = c1->next;
+        }
+        
+        c1 = head, c2 = chead->next;
+        Node *f1, *f2;
+        while(c1) {
+            f1 = c1->next, f2 = c2->next;
+            c1->next = c2;
+            c2->next = f1;
+            c1 = f1;
+            c2 = f2;
+        }
+        
+        c1 = head;
+        while(c1) {
+            c2 = c1->next;
+            if(c1->random) {
+                c2->random = c1->random->next;
+            } else {
+                c2->random = nullptr;
+            }
+            c1 = c2->next;
+        }
+        
+        c1 = head;
+        Node *tail = chead;
+        while(c1) {
+            c2 = c1->next;
+            Node *f = c2->next;
+            tail->next = c2;
+            tail = tail->next;
+            c1->next = f;
+            c1 = f;
+        }
+        
+        return chead->next;
+    }
+};
