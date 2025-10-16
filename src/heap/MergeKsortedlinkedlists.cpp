87fa0da12b09ca1cf1e752c6e190e0acb9830c14
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-16 22:49:31 +0530

    geek heap

--- src/heap/MergeKsortedlinkedlists.cpp
+++ src/heap/MergeKsortedlinkedlists.cpp
@@ -34,3 +34,41 @@ class Solution {
         return head;
     }
 };
+
+
+    /*
+    class Node {
+      public:
+        int data;
+        Node* next;
+    
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    
+    class Solution {
+      public:
+        Node* mergeKLists(vector<Node*>& arr) {
+            priority_queue<pair<int, Node*>, vector<pair<int, Node*>>, greater<pair<int, Node*>>> pq;
+            for(Node* &n : arr) {
+                pq.push({n->data, n});
+            }
+            Node *head = new Node(0);
+            Node *tail = head;
+            while(!pq.empty()) {
+                Node * t = pq.top().second;
+                tail->next = t;
+                if(t->next) {
+                    pq.push({t->next->data, t->next});
+                }
+                tail = tail->next;
+                pq.pop();
+            }
+            
+            tail->next = nullptr;
+            return head->next;
+        }
+    };
