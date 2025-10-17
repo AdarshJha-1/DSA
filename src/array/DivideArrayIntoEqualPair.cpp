6fc06a89d69b55d60c592506f44697b6c781a56c
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-17 12:19:57 +0530

    leetcode array

--- src/array/DivideArrayIntoEqualPair.cpp
+++ src/array/DivideArrayIntoEqualPair.cpp
@@ -9,3 +9,16 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int freq[501];
+        for(int &n : nums) freq[n]++;
+        for(int i = 1; i < 501; i++) {
+            if(freq[i] % 2 != 0) return 0;
+        }
+        return 1;
+    }
+};
