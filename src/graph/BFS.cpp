8b04b3569e5c53f0787e84c0091f2549dfe532ea
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-17 21:10:35 +0530

    geek graph BFS

--- src/graph/BFS.cpp
+++ src/graph/BFS.cpp
@@ -21,3 +21,28 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        vector<int> ans;
+        vector<bool> visited(adj.size(), 0);
+        queue<int> q;
+        q.push(0);
+        visited[0] = 1;
+        while(!q.empty()) {
+            int v = q.front();
+            q.pop();
+            ans.push_back(v);
+            for(int i = 0; i < adj[v].size(); i++) {
+                if(visited[adj[v][i]] == 0) {
+                   visited[adj[v][i]] = 1;
+                   q.push(adj[v][i]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
