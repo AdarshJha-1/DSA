e3ea462bb6934d44d52bd61b2f277d291aec0992
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-19 17:03:33 +0530

    geek graph

--- src/graph/TopologicalSort.cpp
+++ src/graph/TopologicalSort.cpp
@@ -1,3 +1,4 @@
+// DFS Approach
 class Solution {
   public:
     
@@ -31,6 +32,43 @@ class Solution {
         while(!st.empty()) {
             ans.push_back(st.top());
             st.pop();
+        }
+        return ans;
+    }
+};
+
+// BFS Approach
+class Solution {
+  public:
+  
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        
+        vector<vector<int>> adj(V);
+        for(vector<int> &it : edges) {
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        vector<int> ans;
+        vector<int> indgree(V, 0);
+        for(int i = 0; i < V; i++) {
+            for(int j = 0; j < adj[i].size(); j++) {
+                indgree[adj[i][j]]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(indgree[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()) {
+            int n = q.front();
+            q.pop();
+            ans.push_back(n);
+            for(int i = 0; i < adj[n].size(); i++) {
+                indgree[adj[n][i]]--;
+                if(indgree[adj[n][i]] == 0) q.push(adj[n][i]);
+            }
         }
         return ans;
     }
