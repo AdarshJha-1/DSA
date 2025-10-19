13f715d003bf915457859756b342441b3baa9685
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-19 17:48:28 +0530

    leetcode graph same but optimal

--- src/graph/FloodFill.cpp
+++ src/graph/FloodFill.cpp
@@ -31,3 +31,37 @@ public:
         return image;
     }
 };
+
+class Solution {
+public:
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
+        vector<vector<bool>> seen(image.size(), vector<bool>(image[0].size(), 0));
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        seen[sr][sc] = 1;
+        int startColor = image[sr][sc];
+        while(!q.empty()) {
+            const auto&[i, j] = q.front();
+            q.pop();
+            if(image[i][j] == startColor) {
+                image[i][j] = color;
+                seen[i][j] = 1;
+                if(j - 1 >= 0 && !seen[i][j - 1]) {
+                    q.push({i, j - 1});
+                }
+                if(j + 1 < image[i].size() && !seen[i][j + 1]) {
+                    q.push({i, j + 1});
+                }
+                if(i - 1 >= 0 && !seen[i - 1][j]) {
+                    q.push({i -1, j});
+                }
+                if(i + 1 < image.size() && !seen[i + 1][j]) {
+                    q.push({i + 1, j});
+                }
+            }
+        }
+        return image;
+    }
+};
