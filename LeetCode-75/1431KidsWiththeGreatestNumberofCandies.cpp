d035679573162a9d12a00a4c52d81ab1b583240c
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-20 17:43:11 +0530

    leetcode 75 P3

---
+++ LeetCode-75/1431KidsWiththeGreatestNumberofCandies.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans(candies.size(), 0);
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        for(int i = 0; i < candies.size(); i++) {
+            if((candies[i] + extraCandies) >= maxCandies) ans[i] = 1;
+        }
+        return ans;
+    }
+};
