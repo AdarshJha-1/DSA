54a76c07873e028f2ce1f23ff4d3ecea852d0178
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-20 14:00:06 +0530

    geek graph

--- src/graph/DetectCycleDirectedGraph.cpp
+++ src/graph/DetectCycleDirectedGraph.cpp
@@ -25,3 +25,35 @@ class Solution {
         return 0;
     }
 };
+
+
+class Solution {
+  public:
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        vector<int> ind(V, 0);
+        vector<vector<int>> adj(V);
+        for(auto e : edges) {
+            ind[e[1]]++;
+            adj[e[0]].push_back(e[1]);
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(ind[i] == 0) q.push(i);
+        }
+        
+        int c = 0;
+        
+        while(!q.empty()) {
+            int n = q.front();
+            q.pop();
+            c++;
+            for(int i = 0; i < adj[n].size(); i++) {
+                ind[adj[n][i]]--;
+                if(ind[adj[n][i]] == 0) q.push(adj[n][i]);
+            }
+        }
+        
+        return c != V;
+    }
+};
