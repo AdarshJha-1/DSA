ac806be9a833566af6ce4304aca14f56296f0c66
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-20 16:43:05 +0530

    leetcode 75

---
+++ LeetCode-75/1768MergeStringsAlternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+
+        // flag = 0 -> word1
+        // flag = 1 -> word2
+        bool flag = 0; 
+        int i = 0, j = 0;
+        while(i < word1.size() && j < word2.size()) {
+            if(!flag) {
+                ans += word1[i++];
+                flag = !flag;
+            } else {
+                ans += word2[j++];
+                flag = !flag;
+            }
+        }
+        while(i < word1.size()) ans += word1[i++];
+        while(j < word2.size()) ans += word2[j++];
+
+        return ans;
+    }
+};
