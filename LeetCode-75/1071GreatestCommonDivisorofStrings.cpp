25a310b036df4fc8359e866de9d1c01ef690385b
Adarsh Jha <adarsh2005j@gmail.com>
2025-10-20 17:31:55 +0530

    leetcode 75 P2

---
+++ LeetCode-75/1071GreatestCommonDivisorofStrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int gcd(int a, int b) {
+        while(b % a != 0) {
+            int temp = b % a;
+            b = a;
+            a = temp;
+        }
+        return a;
+    }
+    string gcdOfStrings(string str1, string str2) {
+        if((str1 + str2) != (str2 + str1)) return "";
+        int g = gcd(str1.size(), str2.size());
+        string str;
+        for(int i = 0; i < g; i++) {
+            str += str1[i];
+        }
+        return str;
+    }
+};
