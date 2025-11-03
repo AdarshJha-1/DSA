723599961353f65ba6b42c1ce54cc7eb5285bf62
Adarsh Jha <adarsh2005j@gmail.com>
2025-11-03 16:13:32 +0530

    leetcode array Two pointer approach

--- src/array/MinimumTimetoMakeRopeColorful.cpp
+++ src/array/MinimumTimetoMakeRopeColorful.cpp
@@ -11,3 +11,24 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int i = 0, j = 1, ans = 0;
+        while (j < colors.size()) {
+            if (colors[i] == colors[j]) {
+                if (neededTime[i] < neededTime[j]) {
+                    ans += neededTime[i];
+                    i = j;
+                } else {
+                    ans += neededTime[j];
+                }
+            } else {
+                i = j;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
