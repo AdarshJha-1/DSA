6cec0348aa5c3ae3d3108dee7b328a5677876b82
Adarsh Jha <adarsh2005j@gmail.com>
2025-11-22 13:50:08 +0530

    leetcode array

---
+++ src/array/FindMinimumOperationstoMakeAllElementsDivisiblebyThree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ans = 0;
+        for(auto& n : nums) {
+            if(n == 1) ans++;
+            else if(n % 3 != 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
