80156c9581d369ef2c2a24282fc5535081837b44
Adarsh Jha <adarsh2005j@gmail.com>
2025-11-25 22:43:19 +0530

    codeforces

---
+++ codeforces/A_Shizuku_Hoshikawa_and_Farm_Legs.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 1)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << (n / 4) + 1 << endl;
+    }
+}
+
+int32_t main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
