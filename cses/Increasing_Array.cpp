110f6ae49483b3308d8806303292f0eaead0c388
Adarsh Jha <adarsh2005j@gmail.com>
2025-12-12 17:57:01 +0530

    cses

---
+++ cses/Increasing_Array.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            ans += abs(arr[i] - arr[i - 1]);
+            arr[i] = arr[i - 1];
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast_io;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
