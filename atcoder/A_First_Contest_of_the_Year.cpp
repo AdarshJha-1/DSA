735bb99ee5f740f93fb479087d38974af45ffcd0
Adarsh Jha <adarsh2005j@gmail.com>
2026-01-03 20:21:45 +0530

    not much in atcoder

---
+++ atcoder/A_First_Contest_of_the_Year.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    int D, F;
+    cin >> D >> F;
+    int temp = D % 7;
+    int ans = F - temp;
+    if (ans <= 0)
+        ans += 7;
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    fast_io;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
