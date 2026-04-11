5e1426b5d9301b2cefc542f73419d9062ca42703
Adarsh Jha <adarsh2005j@gmail.com>
2026-04-11 22:50:50 +0530

    cses

--- cses/Two_Knights.cpp
+++ cses/Two_Knights.cpp
@@ -11,49 +11,7 @@ using namespace std;
 
 // Observations
 /*
-    get the max between x and y
-    then have maxLimit as max*max;
-    then sub maxLimit till greater than max * 2; -> x wrong
-    find the ele return it;  -> x wrong
 
-    if odd max like in 2, 3 then 3 max
-        do 3 * 3 = 9 maxLimit range
-        we need to find start of it after dry run i found like we can do 3 * 2 - 2
-
-    if even then do in 4, 2 max= 4
-    4 * 4 - 2 -1;
-
-
-    in general term
-    maxE = max(x, y);
-    odd -> ans = max(x, y) * max(x, y) - (max(x, y) * 2 - 2) + (min(x, y) - 1);
-
-    even -> ans = max(x, y) * max(x, y) - (min(x, y) - 1);
-
-    // above approaches are not correct ig?
-
-    if (x > y)
-    {
-        if (x % 2 == 0)
-        {
-            cout << (x * x - (y - 1))
-        }
-        else
-        {
-            cout << (x * x - (x * 2 - 2) + (y - 1))
-        }
-    }
-    else
-    {
-        if (y % 2 == 0)
-        {
-            cout << (y * y - (y * 2 - 2) + (x - 1))
-        }
-        else
-        {
-            cout << (y * y - (x - 1))
-        }
-    }
 */
 
 // Claims on algorithm
@@ -63,30 +21,12 @@ using namespace std;
 
 void solve()
 {
-    int x, y;
-    cin >> x >> y;
+    int N;
+    cin >> N;
 
-    if (x > y)
+    for (int i = 1; i <= N; i++)
     {
-        if (x % 2 == 0)
-        {
-            cout << (x * x - (y - 1)) << endl;
-        }
-        else
-        {
-            cout << (x * x - (x * 2 - 2) + (y - 1)) << endl;
-        }
-    }
-    else
-    {
-        if (y % 2 == 0)
-        {
-            cout << (y * y - (y * 2 - 2) + (x - 1)) << endl;
-        }
-        else
-        {
-            cout << (y * y - (x - 1)) << endl;
-        }
+        int n = i * i;
     }
 }
 
@@ -97,7 +37,7 @@ int32_t main()
     cout.tie(0);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--)
         solve();
 }
