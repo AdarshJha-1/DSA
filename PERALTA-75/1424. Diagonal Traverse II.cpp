2692993ba8f270a1e18fb06403d0474e290129ae
Adarsh Jha <adarsh2005j@gmail.com>
2026-04-12 12:39:38 +0530

    leetcode

---
+++ PERALTA-75/1424. Diagonal Traverse II.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
+    {
+        unordered_map<int, vector<int>> m;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = 0; j < nums[i].size(); j++)
+            {
+                m[(i + j)].push_back(nums[i][j]);
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < m.size(); i++)
+        {
+            for(int j = m[i].size() - 1; j >= 0; j--)
+            {
+                ans.push_back(m[i][j]);
+            }   
+        }
+        return ans;
+    }
+};
