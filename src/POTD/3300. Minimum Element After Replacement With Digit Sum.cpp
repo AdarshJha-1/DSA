f270a414f1347d254009a373393a1b27e1efdaf9
Adarsh Jha <adarsh2005j@gmail.com>
2026-05-29 10:17:57 +0530

    POTD

---
+++ src/POTD/3300. Minimum Element After Replacement With Digit Sum.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int minElement(vector<int>& nums) 
+    {
+        int ans = INT_MAX;
+        for(auto n : nums)
+        {
+            int sum = 0;
+            while(n)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            ans = min(ans, sum);
+        }
+        return ans;
+    }
+};
