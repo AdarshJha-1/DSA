a1e4f5d6f59768f699f4f692fbb9645abfbe7446
Adarsh Jha <adarsh2005j@gmail.com>
2026-06-01 20:55:37 +0530

    segment tree

--- src/segment_tree/SegmentTree.cpp
+++ src/segment_tree/SegmentTree.cpp
@@ -18,6 +18,25 @@ void buildTree(vector<int> &nums, int i, int l, int r)
     return;
 }
 
+void update(int idx, int val, int i, int l, int r)
+{
+    if (l == r)
+    {
+        segT[i] = val;
+        return;
+    }
+    int m = l + (r - l) / 2;
+    if (idx <= m) // to go left -> 2i + 1
+    {
+        update(idx, val, 2 * i + 1, l, m);
+    }
+    else // to go right -> 2i + 2
+    {
+        update(idx, val, 2 * i + 2, m + 1, r);
+    }
+    segT[i] = segT[2 * i + 1] + segT[2 * i + 2];
+}
+
 int main()
 {
     vector<int> nums = {3, 1, 2, 7};
@@ -25,6 +44,14 @@ int main()
     segT.resize(2 * n, 0);
 
     buildTree(nums, 0, 0, n - 1);
+    for (const auto &v : segT)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+
+    update(1, 2, 0, 0, n - 1);
+
     for (const auto &v : segT)
     {
         cout << v << " ";
